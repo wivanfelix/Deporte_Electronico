e5add8b40d2a2ff5b0b060bb17d52f4b221b661c
wivanfelix <wivanfelix@gmail.com>
2019-07-24 22:59:52 -0600

    CRUD Usuarios

--- usuario.js
+++ usuario.js
@@ -11,14 +11,18 @@ module.exports = function userModule(model) {
 
     userRouter.get('/usuarios/:id', function (req, res) {
         var id = req.params.id;
-        model.Usuario.findOne(id).then(function(usuario){
-            if(usuario){
+        model.Usuario.findOne({
+            where: {
+                id: id
+            }
+        }).then(function (usuario) {
+            if (usuario) {
                 res.json(usuario);
-            }else{
+            } else {
                 res.status(400).json();
             }
         });
-        });
+    });
 
 
     userRouter.post('/usuarios', function (req, res) {
@@ -30,27 +34,33 @@ module.exports = function userModule(model) {
         });
 
 
-    userRouter.put('/usuarios', function (req, res) {
-        var input = req.body;
-        var input = req.params.id;
-        model.Usuario.update(input,{
-            where:{
-                id: id
-            }
-        }).then(function(usuario){
-            res.json(usuario);
+        userRouter.put('/usuarios/:id', function (req, res) {
+            var input = req.body;
+            var id = req.params.id;
+            model.Usuario.update({
+                nombre_Usuario: input.nombre_Usuario,
+                edad_Usuario: input.edad_Usuario,
+                usuario_Usuario: input.usuario_Usuario,
+                contrasena_Usuario: input.contrasena_Usuario,
+                tipo_usuario: input.tipo_usuario,
+                pais_Usuario: input.pais_Usuario
+            }, {
+                    where: {
+                        id: id
+                    }
+                }).then(function (usuario) {
+                    res.json(usuario);
+            });
         });
-
-
-        model.Usuario.create(inpu).then(function(usuario){
-            res.json(usuario);
-        });
-    });
 
 
     userRouter.delete('/usuarios/:id', function (req, res) {
         var id = req.params.id;
-        model.Usuario.destroy(id).then(function(usuario){
+        model.Usuario.destroy({
+            where: {
+                id: id
+            }
+        }).then(function(usuario){
             res.json(usuario);
         });
     });
